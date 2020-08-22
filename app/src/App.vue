35ef4aa042722342058f5a676391790b15eb4c6f
Bhon Bunnag <bunnag.bn@gmail.com>
2020-08-23 00:46:42 +0700

    Added an iframe

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="height:600px">
+    <iframe id="frame" src="https://lichess.org/embed/XIwZa926" title="W3Schools Free Online Web Tutorials"></iframe>
+    </div>
   </div>
 </template>
 
@@ -24,5 +26,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#frame {
+   width: 100%;
+   height: 100%;
+   border: 1px solid black;
+   position: relative;
 }
 </style>
