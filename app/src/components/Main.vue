0aad25d3bac5935316b742a5006274a8f3dcf281
Bhon Bunnag <bunnag.bn@gmail.com>
2020-08-23 23:47:53 +0700

    add buttons to move to next/prev game

--- app/src/components/Main.vue
+++ app/src/components/Main.vue
@@ -1,6 +1,8 @@
 <template>
   <div style="height:600px">
     <iframe id="frame" :src="urlGenerator" title="W3Schools Free Online Web Tutorials"></iframe>
+    <button @click="prev">Prev</button>
+    <button @click="next">Next</button>
   </div>
 </template>
 
@@ -17,7 +19,14 @@ export default {
       return `https://lichess.org/embed/${gamePos.id}/${gamePos.color}#${gamePos.moveNum}`
     }
   },
-  methods: {}
+  methods: {
+    next: function () {
+      this.currGame = (this.currGame + 1) % this.games.length
+    },
+    prev: function () {
+      this.currGame = (this.currGame + this.games.length - 1) % this.games.length
+    }
+  }
 }
 </script>
 
